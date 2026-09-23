6b06f39a5c2c02a0f784d4ae5a51082f75787f73
thanadolnar-png <thanadol.nar@gmail.com>
2026-09-23 16:41:24 +0700

    update: sync quiz database Wed 09/23/ 16:41

--- quiz-data-offline.js
+++ quiz-data-offline.js
@@ -1,7 +1,7 @@
 /**
  * 📝 PLE-CC Quiz Practice -- Auto-compiled Offline Database (Rich Text & Column O)
  * Total Questions: 76 across 7 Categories
- * Build Timestamp: 2026-09-23 16:29:31
+ * Build Timestamp: 2026-09-23 16:41:23
  */
 
 window.QUIZ_GOOGLE_SHEET_ID = "1CaIHXpiiAi8tFFX2IGXwXp2rXUv6JaOMiKBAiVpAV0w";
